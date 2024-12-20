9f123a4c28cc9248467703e860e37a25270ead24
shibaisdog <kimjaehoon5142@gmail.com>
2024-12-20 17:46:47 +0900

    There is opacity in the subtitles

--- src/gui.py
+++ src/gui.py
@@ -210,9 +210,35 @@ def render_subtitles(subtitles):
             pass
     for content in current_subtitle:
         #print(f"{content['position']} / {content['line']} / {content['text']}")
-        text_surface = src.win.screen.font.render(content['text'], True, (236,82,82))
-        text_rect = text_surface.get_rect(center=(src.win.screen.win.get_width() * (int(content['position']) / 100), src.win.screen.win.get_height() * (int(content['line']) / 100)))
-        src.win.screen.win.blit(text_surface, text_rect)
+        if '\n' in content['text']:
+            i = 0
+            for line in (content['text'].split('\n'))[::-1]:
+                text_surface = src.win.screen.font.render(line, True, (236,82,82))
+                text_rect = text_surface.get_rect(center=(src.win.screen.win.get_width() * (int(content['position']) / 100), src.win.screen.win.get_height() * (int(content['line']) / 100) - i * 30))
+                padding = 2
+                rect_width = text_rect.width + padding * 2
+                rect_height = text_rect.height + padding * 2
+                rect_x = text_rect.x - padding
+                rect_y = text_rect.y - padding
+                rectangle_surface = pygame.Surface((rect_width, rect_height))
+                rectangle_surface.fill((0, 0, 0))
+                rectangle_surface.set_alpha(128)
+                src.win.screen.win.blit(rectangle_surface, (rect_x, rect_y))
+                src.win.screen.win.blit(text_surface, text_rect)
+                i += 1
+        else:
+            text_surface = src.win.screen.font.render(content['text'], True, (236,82,82))
+            text_rect = text_surface.get_rect(center=(src.win.screen.win.get_width() * (int(content['position']) / 100), src.win.screen.win.get_height() * (int(content['line']) / 100)))
+            padding = 2
+            rect_width = text_rect.width + padding * 2
+            rect_height = text_rect.height + padding * 2
+            rect_x = text_rect.x - padding
+            rect_y = text_rect.y - padding
+            rectangle_surface = pygame.Surface((rect_width, rect_height))
+            rectangle_surface.fill((0, 0, 0))
+            rectangle_surface.set_alpha(128)
+            src.win.screen.win.blit(rectangle_surface, (rect_x, rect_y))
+            src.win.screen.win.blit(text_surface, text_rect)
     #pygame.display.flip()
 
 def run(url: str, seek = 0):
